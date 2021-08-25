57ed1bd663da69292583d298eda5a5650e667c93
Miguel Teheran <miguelteheran@gmail.com>
2021-08-24 20:56:22 -0500

    swagger documentation

--- Program.cs
+++ Program.cs
@@ -3,13 +3,16 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Hosting;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(); 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
 }
+app.UseSwagger();
 
 app.MapGet("/", (Func<string>)(() => "Hello World!"));
-
+app.UseSwaggerUI();
 app.Run();
